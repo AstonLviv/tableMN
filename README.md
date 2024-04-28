f80fb7eb5a4300ef0f34cefc4c2d287e0b5029e0
AstonLviv <aston.lviv@gmail.com>
2024-04-28 14:14:52 +0300

    add mobs

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 # tableMN
-
-+ mine rss
-+ mine rss count
-+ chance to mine
-+ mine skill impove
-+ skill stats
-+ shop (forge)
-+ craft
-+ fix mine bug (rss count is not udated)
-+ fix craft bug (rss name wrong)
-+ inventory
-- mobs
-- mob drop
-- hp (amount + regen)
-- attack
-- level
-- skills
-- players
-- multiplayer 
-- lan game
-
-* upgrade skillLevel with rss
+MINE,RSS:
+	+ mine rss
+	+ mine rss count
+	+ chance to mine
+	+ mine skill impove
+	+ skill stats
+	+ shop (forge)
+	+ craft
+	+ fix mine bug (rss count is not udated)
+	+ fix craft bug (rss name wrong)
+	+ inventory
+FIGHTING:
+	- mobs
+	- mob drop must going to inventory
+	- gears
+	- hp (amount + regen)
+	- attack
+	- level
+	- skills
+MULTYPLAYER:
+	- players
+	- multiplayer 
+	- lan game
---
+++ mobs.js
@@ -0,0 +1,177 @@
+let mobs = [
+	{	
+		name: 	"spider",
+		hp: 	4,
+		description: "a big spider that lives in a cave",
+		damage: 1,
+		xp: 1,
+		loot: [
+			{
+				name: "hp",
+				value: 1,
+				dice: 4
+			},
+			{
+				name: "potion",
+				value: 1,
+				dice: 5
+			},
+			{
+				name: "dice",
+				value: 1,
+				dice: 6
+			}
+		]		
+	},
+	{	
+		name: 	"zombie",
+		hp: 	8,
+		description: "a zombie that raise from a grave at night",
+		damage: 1.5,
+		xp: 3,
+		loot: [
+			{
+				name: "hp",
+				value: 2,
+				dice: 2
+			},
+			{
+				name: "hp",
+				value: 3,
+				dice: 3
+			},
+			{
+				name: "potion",
+				value: 1,
+				dice: 4
+			},
+			{
+				name: "dice",
+				value: 1,
+				dice: 5
+			},
+			{
+				name: "dice and potion",
+				value: 1,
+				dice: 6
+			}
+		]	
+	},
+	{	
+		name: 	"skeleton",
+		hp: 	12,
+		description: "skeleton king raised this skeleton to defeat you",
+		damage: 2.5,
+		xp: 5,
+		loot: [
+			{
+				name: "hp",
+				value: 3,
+				dice: 1
+			},
+			{
+				name: "hp",
+				value: 3,
+				dice: 2
+			},
+			{
+				name: "potion",
+				value: 2,
+				dice: 3
+			},
+			{
+				name: "dice",
+				value: 2,
+				dice: 4
+			},
+			{
+				name: "dice and potion",
+				value: 1,
+				dice: 5
+			},
+			{
+				name: "dice and potion",
+				value: 1,
+				dice: 6
+			}
+		]		
+	},
+	{	
+		name: 	"blaze",
+		hp: 	20,
+		description: "a burning skeleton that can throw fireballs and fly",
+		damage: 4,
+		xp: 20,
+		loot: [
+			{
+				name: "hp",
+				value: 5,
+				dice: 1
+			},
+			{
+				name: "hp",
+				value: 5,
+				dice: 2
+			},
+			{
+				name: "potion",
+				value: 4,
+				dice: 3
+			},
+			{
+				name: "dice",
+				value: 3,
+				dice: 4
+			},
+			{
+				name: "dice and potion",
+				value: 2,
+				dice: 5
+			},
+			{
+				name: "dice and potion",
+				value: 2,
+				dice: 6
+			}
+		]	
+	},
+	{
+		name: 	"Skeleton King",
+		hp: 	35,
+		description: "THE FINAL BOSS",
+		damage: 7.5,
+		xp: 50,
+		loot: [
+			{
+				name: "hp",
+				value: 6,
+				dice: 1
+			},
+			{
+				name: "hp",
+				value: 7,
+				dice: 2
+			},
+			{
+				name: "potion",
+				value: 4,
+				dice: 3
+			},
+			{
+				name: "dice and potion",
+				value: 3,
+				dice: 4
+			},
+			{
+				name: "dice and potion",
+				value: 4,
+				dice: 5
+			},
+			{
+				name: "dice and potion",
+				value: 5,
+				dice: 6
+			}
+		]	
+	}
+]
